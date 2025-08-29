55ffa053728b94ed04723ccb0d463f73f66ea014
anthony-63 <back.up.email.158@gmail.com>
2025-08-28 23:23:24 -0400

    hello project

---
+++ README.md
@@ -0,0 +1,5 @@
+# Acheron
+The all-in-one Game Developemnt Framework. Complete with an ECS and a high-level Graphics API.
+
+### INFO
+Staying private for now until established.
---
+++ shell.nix
@@ -0,0 +1,24 @@
+{ pkgs ? import <nixpkgs> {} }:
+  pkgs.mkShell {
+    nativeBuildInputs = with pkgs.buildPackages; [
+      libgcc
+      meson
+      cmake
+      ninja
+      xorg.libX11
+      xorg.libXrandr
+      xorg.libXinerama
+      xorg.libXcursor
+      xorg.libXi
+      xorg.libxcb
+      xorg.xeyes
+      glfw
+      libGL
+      gdb
+    ];
+    shellHook = ''
+      export LD_LIBRARY_PATH=${pkgs.xorg.libX11}/lib:${pkgs.libGL}/lib:${pkgs.glfw}/lib:$LD_LIBRARY_PATH
+    '';
+
+    hardeningDisable = [ "fortify" ];
+}
