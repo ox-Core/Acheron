e3ed4a93b4e509d6515d388afe5d36b59f7296f2
anthony-63 <back.up.email.158@gmail.com>
2025-09-01 15:36:12 -0400

    new example, for testing

---
+++ examples/ecs/components.cpp
@@ -0,0 +1,22 @@
+#include <print>
+
+#include "acheron.hpp"
+
+using namespace acheron;
+
+struct TestComponent {
+    bool dummy = false;
+};
+
+int main() {
+    auto world = ecs::World();
+
+    std::println("Register start");
+    world.RegisterComponent<TestComponent>();
+    std::println("Register end");
+
+    std::println("spawn start");
+    world.Spawn()
+        .Add<TestComponent>();
+    std::println("spawn end");
+}
