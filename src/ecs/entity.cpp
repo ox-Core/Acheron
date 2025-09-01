6c6926b09efceb94eecbd2e858b4234c5c9cba26
anthony-63 <back.up.email.158@gmail.com>
2025-09-01 16:25:25 -0400

    temporary fix

--- src/ecs/entity.cpp
+++ src/ecs/entity.cpp
@@ -8,28 +8,7 @@ using namespace acheron::ecs;
 EntityManager::EntityManager() : idCounter(0) {}
 
 Entity EntityManager::Spawn() {
-    assert(idCounter >= 0); // if signed
-    Entity entity = Entity(-1);
-
-    if (!availableEntities.empty()) {
-        entity = availableEntities.back();
-        // quick invariants:
-        assert(entity != Entity(-1));
-        assert(entity < 1000000000u); // large sentinel to catch garbage
-        availableEntities.pop_back();
-    } else {
-        entity = static_cast<Entity>(idCounter++);
-        assert(entity != Entity(-1));
-    }
-
-    if (signatures.find(entity) != signatures.end()) {
-        // should not happen for a freshly spawned entity
-        assert(false && "spawning an entity that already exists in signatures");
-    }
-
-    signatures.emplace(entity, Signature{});
-
-    return entity;
+    return idCounter++;
 }
 
 void EntityManager::Despawn(Entity entity) {
