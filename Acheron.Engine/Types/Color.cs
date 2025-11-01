f005ffb78ab49ded9bbd0d56a67cb9a899af7f10
talonateox <back.up.email.158@gmail.com>
2025-11-01 02:46:03 -0400

    Color Wrapper

--- Acheron.Engine/Types/Color.cs
+++ Acheron.Engine/Types/Color.cs
@@ -1,27 +1,30 @@
+using System.Drawing;
+using System.Numerics;
 using Acheron.Core.ECS;
 
 namespace Acheron.Engine.Types;
 
 [Component]
-public class Color {
-    public byte r = 0, g = 0, b = 0, a = 255;
-
-    public Color() { }
-    public Color(byte r, byte g, byte b, byte a) {
-        this.r = r;
-        this.g = g;
-        this.b = b;
-        this.a = a;
+public class ColorWrapper {
+    public static Color From(byte r, byte g, byte b, byte a) {
+        return Color.FromArgb(a, r, g, b);
     }
 
-    public Color(uint hex) {
-        r = (byte)((hex & 0xff000000) >> 24);
-        g = (byte)((hex & 0x00ff0000) >> 16);
-        b = (byte)((hex & 0x0000ff00) >> 8);
-        a = (byte)((hex & 0x000000ff) >> 0);
+    public static Color From(uint hex) {
+        var r = (byte)((hex & 0xff000000) >> 24);
+        var g = (byte)((hex & 0x00ff0000) >> 16);
+        var b = (byte)((hex & 0x0000ff00) >> 8);
+        var a = (byte)((hex & 0x000000ff) >> 0);
+
+        return Color.FromArgb(a, r, g, b);
     }
 
-    public float[] ToFloat() {
-        return [r / 255.0f, g / 255.0f, b / 255.0f, a / 255.0f];
+    public static float[] ToFloat(Color color) {
+        return [color.R / 255.0f, color.G / 255.0f, color.B / 255.0f, color.A / 255.0f];
     }
+
+    public static Vector4 ToVec4(Color color) {
+        var colorArr = ToFloat(color);
+        return new Vector4(colorArr[0], colorArr[1], colorArr[2], colorArr[3]);
+    } 
 }
