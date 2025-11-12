19c1075768b55facad7549f9bd370af729b6a596
talonateox <back.up.email.158@gmail.com>
2025-11-12 07:14:52 -0500

    Material hashing

--- Acheron.Engine/Renderer/Material.cs
+++ Acheron.Engine/Renderer/Material.cs
@@ -1,5 +1,7 @@
 using System.Drawing;
+using System.Security.Cryptography;
 using Acheron.Core.ECS;
+using Acheron.Engine.Types;
 
 namespace Acheron.Engine.Renderer;
 
@@ -7,4 +9,14 @@ namespace Acheron.Engine.Renderer;
 public class Material {
     public Shader Shader = Shader.Invalid;
     public Color Color = Color.White;
+    public Texture2D Texture = new();
+
+    private string? id = null;
+
+    public string ID {
+        get {
+            id ??= SHA1.HashData([(byte)Shader.ID, (byte)Texture.Handle, Color.R, Color.G, Color.B, Color.A]).ToString();
+            return id!;
+        }
+    }
 }
