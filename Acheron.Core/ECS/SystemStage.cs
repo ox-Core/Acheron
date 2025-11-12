6b365c103bb252666e2d84a4995afeb9f0043549
talonateox <back.up.email.158@gmail.com>
2025-11-12 07:09:07 -0500

    Add stage ids

--- Acheron.Core/ECS/SystemStage.cs
+++ Acheron.Core/ECS/SystemStage.cs
@@ -1,8 +1,8 @@
 namespace Acheron.Core.ECS;
 
-public class Stage
-{
+public class Stage {
     public string Name { get; }
+    public int ID { get; }
     public List<ISystem> Systems { get; } = [];
     public Stage(string name) => Name = name;
 }
